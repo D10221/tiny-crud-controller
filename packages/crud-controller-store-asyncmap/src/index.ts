010ad8de61d4b12f82b1d3b49070819f7ca9fa0b
Daniel <danieliozzi@gmail.com>
2018-12-01 08:45:41 +0200

    ...

---
+++ packages/crud-controller-store-asyncmap/src/index.ts
@@ -0,0 +1,29 @@
+import { Store } from "@australis/tiny-crud-controller";
+
+export default class MapStore<T extends {} = {}> implements Store {
+  map = new Map<string, T>();
+  find = (key: string) => {
+    if (key) return Promise.resolve(this.map.get(key));
+    const out: any[] = [];
+    this.map.forEach((value, key) => {
+      out.push({ key, ...(value || {}) });
+    });
+    return Promise.resolve(out);
+  };
+  update = (key: string, value: T) => {
+    if (!this.map.has(key))
+      return Promise.reject(new Error("Not Found, Key: " + key));
+    this.map.set(key, value);
+    return Promise.resolve();
+  };
+  add = (key: string, value: T) => {
+    if (this.map.has(key))
+      return Promise.reject(new Error("Duplicated key: " + key));
+    return Promise.resolve(this.map.set(key, value));
+  };
+  remove = (key: string) => {
+    if (!this.map.has(key))
+      return Promise.reject(new Error("Not Found, Key: " + key));
+    return Promise.resolve(this.map.set(key, undefined));
+  };
+}
---
+++ scripts/add-package.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const runCmd = require("./run-cmd");
+async function run() {
+  try {
+    for (const arg of process.argv.slice(2)) {
+      await runCmd(`yarn`, ["add", arg]);
+      await runCmd(`yarn`, ["add", `@types/${arg}`, '-D']);
+    }
+    return "OK";
+  } catch (error) {      
+      return Promise.reject(error);
+  }
+}
+run().then(console.log).catch(e=> {
+    console.error(e);
+    process.exit(-1);
+})
