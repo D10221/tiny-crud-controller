9bd29731447bc88c277b5803f6049649e73b61f8
Daniel <danieliozzi@gmail.com>
2018-12-04 04:06:19 +0200

    fmt

--- packages/crud-controller-store-level/src/index.ts
+++ packages/crud-controller-store-level/src/index.ts
@@ -1,10 +1,14 @@
-
 import levelup from "levelup";
 import jsonDown from "jsondown";
 import { join, isAbsolute } from "path";
 
-export const newLevel = (location: string) => levelup(jsonDown(isAbsolute(location) ? location : join(process.cwd(), location)));
-
+export const newLevel = (location: string) =>
+  levelup(
+    jsonDown(isAbsolute(location) ? location : join(process.cwd(), location)),
+  );
+/**
+ * value serializer
+ */
 const _serializer = {
   serialize: JSON.stringify,
   deserialize(value: Buffer | string) {
@@ -19,70 +23,75 @@ const _serializer = {
     }
     throw new Error("Not Implemented");
   },
-}
-/** */
+};
+/**
+ * Key encoder
+ */
 const encoder = (name: string) => {
   const regex = new RegExp(`^${name}\/.*`, "i");
   return {
     isMatch(key: Buffer | string) {
-      if (typeof key === "string")
-        return regex.test(key)
+      if (typeof key === "string") return regex.test(key);
       return key.toString();
-    }
-    ,
+    },
     decode(key: string | Buffer) {
       if (typeof key === "string") return key.split(`${name}/`)[1];
       return key.toString().split(`${name}/`)[1];
-    }
-    ,
+    },
     encode(id: string) {
-      return `${name}/${id}`
-    }
-  }
-}
+      return `${name}/${id}`;
+    },
+  };
+};
 
 export type Serializer = {
-  serialize: (a: any) => string,
-  deserialize: (value: string | Buffer) => any
+  serialize: (a: any) => string;
+  deserialize: (value: string | Buffer) => any;
 };
 
 export type Encoder = {
-  encode(s: string): string,
-  decode(s: string): string,
+  encode(s: string): string;
+  decode(s: string): string;
   isMatch: (s: string | Buffer) => boolean;
-}
+};
 export interface Options {
-  serializer?: Serializer,
-  encoder?: Encoder,
+  serializer?: Serializer;
+  encoder?: Encoder;
   map?: {
-    out?: (key: string, value: any) => any,
-    in?: (x: any) => any
-  }
+    out?: (key: string, value: any) => any;
+    in?: (x: any) => any;
+  };
 }
 /** */
 const catchNotFound = (returns: any = null) => (error: Error) => {
-  return error && error.name === "NotFoundError" ? returns : Promise.reject(error)
-}
+  return error && error.name === "NotFoundError"
+    ? returns
+    : Promise.reject(error);
+};
 /**
- * './mydata.json'
+ * 
  */
 export default (name: string) => {
-
   return (db: any = null, options?: Options) => {
-
     db = db || newLevel("store.json");
 
-    const { serialize, deserialize } = options && options.serializer || _serializer;
-    const { encode, decode, isMatch } = (options && options.encoder) || encoder(name);
-    const mapOut = options && options.map && options.map.out || ((key, value) => ({ key, ...(value || {}) }));
-    const mapIn = options && options.map && options.map.in || ((x: any) => x);
+    const { serialize, deserialize } =
+      (options && options.serializer) || _serializer;
+    const { encode, decode, isMatch } =
+      (options && options.encoder) || encoder(name);
+    const mapOut =
+      (options && options.map && options.map.out) ||
+      ((key, value) => ({ key, ...(value || {}) }));
+    const mapIn = (options && options.map && options.map.in) || ((x: any) => x);
     const find = async (id?: string) => {
       if (id) {
-        return db.get(encode(id)).then((value: any) => mapOut(id, deserialize(value)));
+        return db
+          .get(encode(id))
+          .then((value: any) => mapOut(id, deserialize(value)));
       } else {
         return new Promise((resolve, reject) => {
           try {
-            const stream = (db.createReadStream() as NodeJS.ReadableStream);
+            const stream = db.createReadStream() as NodeJS.ReadableStream;
             let result: any[] = [];
             stream.on("data", ({ key, value }) => {
               if (isMatch(key)) {
@@ -91,11 +100,11 @@ export default (name: string) => {
             });
             stream.on("error", error => {
               reject(error);
-            })
+            });
             // stream.on("close", () => {});
             stream.on("end", () => {
               resolve(result);
-            })
+            });
           } catch (error) {
             return reject(error);
           }
@@ -105,38 +114,41 @@ export default (name: string) => {
     /** */
     return {
       add: async (id: string, data: {}) => {
-        if (await find(id).catch(catchNotFound(false))) return Promise.reject(new Error(`Duplicate key: ${id}`));
-        return db.put(encode(id), serialize(mapIn(data))) as Promise<any>
+        if (await find(id).catch(catchNotFound(false)))
+          return Promise.reject(new Error(`Duplicate key: ${id}`));
+        return db.put(encode(id), serialize(mapIn(data))) as Promise<any>;
       },
       find,
       remove(id?: string) {
         return db.del(encode(id));
       },
       update: async (id: string, data: {}) => {
-        if (!await find(id)) return Promise.reject(new Error(`Not Found key: ${id}`));
+        if (!(await find(id)))
+          return Promise.reject(new Error(`Not Found key: ${id}`));
         return db.put(encode(id), serialize(mapIn(data)));
       },
-      clear: () => new Promise((resolve, reject) => {
-        try {
-          const stream = (db.createReadStream() as NodeJS.ReadableStream);
-          let result = 0;
-          stream.on("data", async ({ key }) => {
-            if (isMatch(key)) {
-              result = result + 1
-              await db.del(key);
-            }
-          });
-          stream.on("error", error => {
-            reject(error);
-          })
-          // stream.on("close", () => {});
-          stream.on("end", () => {
-            resolve(result);
-          })
-        } catch (error) {
-          return reject(error);
-        }
-      })
+      clear: () =>
+        new Promise((resolve, reject) => {
+          try {
+            const stream = db.createReadStream() as NodeJS.ReadableStream;
+            let result = 0;
+            stream.on("data", async ({ key }) => {
+              if (isMatch(key)) {
+                result = result + 1;
+                await db.del(key);
+              }
+            });
+            stream.on("error", error => {
+              reject(error);
+            });
+            // stream.on("close", () => {});
+            stream.on("end", () => {
+              resolve(result);
+            });
+          } catch (error) {
+            return reject(error);
+          }
+        }),
     };
-  }
+  };
 };
